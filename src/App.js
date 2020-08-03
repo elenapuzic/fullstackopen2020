ca27e9b13168169baa09241906f909d38b76208b
elenapuzic <61571552+elenapuzic@users.noreply.github.com>
2020-08-03 22:05:18 +0300

    1.3-1.4 done

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ import Total from "./components/Total";
 
 const App = () => {
     const course = 'Half Stack application development'
-    const contentText = [
-        { part: 'Fundamentals of React', exercises: 10 },
-        { part: 'Using props to pass data', exercises: 7 },
-        { part: 'State of a component', exercises: 14 }
+    const parts = [
+        {
+            part: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            part: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            part: 'State of a component',
+            exercises: 14
+        }
     ];
 
     return (
         <div>
             <Header courseName={course} />
-            <Content partArray={contentText}/>
-            <Total numOfExercises={contentText} />
+            <Content partArray={parts}/>
+            <Total numOfExercises={parts} />
         </div>
     )
 }
